b8c9debe6ef69baf58cd9da82ca408ae1108283a
fofe <b.fofe@yahoo.com>
2020-01-14 10:39:47 +0100

    ok interface

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,65 @@
 import { Component } from '@angular/core';
 import { Media, MediaObject } from '@ionic-native/media';
 
+export interface Track{
+  name:string;
+  path:string;
+  playing:boolean;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  tracks: any;
+  tracks:Track[]=[
+    {
+      name:"Africa",
+      path:"./assets/mp3/Africa.mp3",
+      playing:false,
+    },
+    {
+      name:"bore",
+      path:"./assets/mp3/He bore.mp3",
+      playing:false,
+    },
+    {
+      name:"le menusier de ta vie",
+      path:"./assets/mp3/le menusier de ta vie.mp3",
+      playing:false,
+    },
+    {
+      name:"No vamos",
+      path:"./assets/mp3/No vamos.mp3",
+      playing:false,
+    },
+    {
+      name:"Pire",
+      path:"./assets/mp3/pire.mp3",
+      playing:false,
+    },
+    {
+      name:"Suze dob",
+      path:"./assets/mp3/Suze dob.mp3",
+      playing:false,
+    },
+    {
+      name:"Ta vie dose",
+      path:"./assets/mp3/Ta vie dose.mp3",
+      playing:false,
+    },
+    {
+      name:"Wale",
+      path:"./assets/mp3/Wale.mp3",
+      playing:false,
+    },
+    {
+      name:"Wassa",
+      path:"./assets/mp3/Wassa.mp3",
+      playing:false,
+    }
+  ];
   playing: boolean = true;
   currentTrack: any;
   progressInterval: any;
@@ -15,7 +67,7 @@ export class HomePage {
 
   constructor( private media: Media) {
 
-    this.tracks = [
+   /* this.tracks = [
       {title: 'DAMSO', artist: 'best', playing: false, progress: 0,path:"1.mp3"},
       {title: 'Damso2', artist: 'Allison Wonderland', playing: false, progress: 0,path:"2.mp3"},
       {title: 'Breathe', artist: 'Seeb Neev', playing: false, progress: 0,path:"1.mp3"},
@@ -24,7 +76,7 @@ export class HomePage {
       {title: 'Stay High', artist: 'Tove Lo', playing: false, progress: 0,path:"1.mp3"},
       {title: 'Lean On', artist: 'Major Lazer', playing: false, progress: 0,path:"1.mp3"},
       {title: 'Damso', artist: 'Damso', playing: false, progress: 0,path:"2.mp3"}
-    ];
+    ];*/
 
     this.currentTrack = this.tracks[0];
 
